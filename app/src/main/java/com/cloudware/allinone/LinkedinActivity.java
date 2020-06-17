2e7b085ae5d5970150f521b148fafe611938e46d
ade <adereal200@gmail.com>
2020-06-17 03:30:29 +0100

    initial commit

---
+++ app/src/main/java/com/cloudware/allinone/LinkedinActivity.java
@@ -0,0 +1,84 @@
+package com.cloudware.allinone;
+
+import android.app.ProgressDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+public class LinkedinActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout refreshLayout;
+    @RequiresApi(api = Build.VERSION_CODES.P)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linkedin);
+        getSupportActionBar().hide();
+
+        refreshLayout = findViewById(R.id.swipe);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Data...");
+        progressDialog.setCancelable(false);
+        final WebView web_view = findViewById(R.id.web_view_linkedin);
+        web_view.requestFocus();
+
+        web_view.getSettings().setJavaScriptEnabled(true);
+
+
+        web_view.getSettings().setAppCachePath(getApplicationContext().getFilesDir().getAbsolutePath() + "/cache");
+        web_view.getSettings().setDatabasePath(getApplicationContext().getFilesDir().getAbsolutePath() + "/databases");
+        web_view.loadUrl("https://www.linkedin.com/");
+        web_view.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        web_view.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+
+                if (progress < 100) {
+                    progressDialog.show();
+                }
+                if (progress == 100) {
+                    progressDialog.dismiss();
+                }
+            }
+        });
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshLayout.setRefreshing(true);
+                web_view.reload();
+                refreshLayout.setRefreshing(false);
+            }
+        });
+
+        web_view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction()
+                        == MotionEvent.ACTION_UP && web_view.canGoBack()) {
+                    web_view.goBack();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
